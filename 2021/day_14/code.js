9f52a9ad3c9083d1d806a038ca80edd42dabcfde
Martin Fučík <mar.fucik@gmail.com>
2021-12-14 01:51:29 +0100

    Preparation for Day 14

---
+++ 2021/day_14/code.js
@@ -0,0 +1,16 @@
+fs = require('fs')
+const DATA = fs.readFileSync('data.txt', {encoding:'utf8'});
+
+const LINES = DATA.split('\n').map(a => a.trim())
+
+part1();
+part2();
+
+function part1() {
+
+}
+
+function part2() {
+
+   
+}
